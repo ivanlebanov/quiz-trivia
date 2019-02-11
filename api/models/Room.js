964a307af1d1d19aecfdb8d3c07a08eb74cf3e81
Alan Jones <jonezzyboy@gmail.com>
2019-02-11 12:03:49 +0000

    Create room send request
    Added room code attribute to be generated by shortid

--- api/models/Room.js
+++ api/models/Room.js
@@ -1,9 +1,15 @@
 var mongoose = require('mongoose')
+var shortid = require('shortid')
 
 var roomSchema = mongoose.Schema({
   name: {
     type: String,
     required: true
+  },
+  code: {
+    type: String,
+    required: true,
+    default: shortid.generate
   }
 })
 
