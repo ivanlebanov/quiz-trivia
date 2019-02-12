86f93996a9fe92d94b201bf02d267e85c8ea80f1
Ivan Lebanov <ivan.lebanov@thejustbrand.com>
2019-02-12 15:26:15 +0000

    join game method

--- client/src/store/modules/room/actions.js
+++ client/src/store/modules/room/actions.js
@@ -14,10 +14,32 @@ export default {
       .then(room => {
         router.push({
           name: 'RoomLobby',
-          params: {code: room.data.code}
+          params: {
+            code: room.data.code
+          }
         })
       })
       .catch(r => {
+        //dispatch("user/logout", null, { root:true })
+      })
+  },
+  async joinGame({
+    state,
+    commit,
+    dispatch
+  }, data) {
+    axios.put('http://localhost:3000/room', data)
+      .then(room => {
+        alert();
+        router.push({
+          name: 'RoomLobby',
+          params: {
+            code: room.data.code
+          }
+        })
+      })
+      .catch(r => {
+        alert('error');
         //dispatch("user/logout", null, { root:true })
       })
   }
